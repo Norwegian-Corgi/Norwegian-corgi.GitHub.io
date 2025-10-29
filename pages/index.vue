8e67c9cf127bebabb0378a1b343899ea14ffa2c1
norwegian-corgi <abdealisabun8@gmail.com>
2025-10-29 10:03:51 +0100

    Make copyright enddate dynamic.

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,11 @@
 <template>
   <main class="bg-blue-custom text-white">
-    <main-section id="home"/>
-    <about-me-section id="about-me"/>
-    <career-section id="career"/>
-    <social-links id="social-links"/>
-    <footer class="flex justify-center py-8">© 2023-2024 Abde Ali Sabunwala</footer>
+    <main-section id="home" />
+    <about-me-section id="about-me" />
+    <career-section id="career" />
+    <social-links id="social-links" />
+    <footer class="flex justify-center py-8">
+      © 2023-{{ new Date().getFullYear() }} Abde Ali Sabunwala
+    </footer>
   </main>
 </template>
